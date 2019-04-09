7064e45203f0371174f5ba19439e3755b031cd1b
James Phong Duong <james.duong93@gmail.com>
2019-04-09 19:21:46 +1000

    types and data

--- app.js
+++ app.js
@@ -4,20 +4,39 @@ const graphQLHTTP = require('express-graphql'); //imports a function used where 
 const { buildSchema } = require('graphql');
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use(
   '/graphql',
   //schema has two properties: query & mutation
   //graphql is a typed language, define the query and mutation (can call w/e) and declare the type and link it via the schema (bundles the query and mutations)
+  //type Event e.g. we have an Event with property (id) and type of data (ID!) - GRAPHQL syntax, exclation means it must never be null, i.e. ID should always be given
+  //input is input type used as a list of individual argumentstbone
   graphQLHTTP({
     schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput { 
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
     }
 
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -28,11 +47,18 @@ app.use(
     //resolvers - logic aka controllers, need to follow the same name as query and mutation objects
     rootValue: {
       events: () => {
-        return ['Romantic Cooking', 'Sailing', 'All-night Coding'];
+        return events;
       },
       createEvent: args => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date
+        };
+        events.push(event);
+        return event;
       }
     },
     graphiql: true // provides us graphql interface
